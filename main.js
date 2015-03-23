69c2d13f886760f34353c2defe04405c36bc3cd8
the-AjK <alberto.garbui@gmail.com>
2015-03-23 12:00:19 +0100

    first release 1.0

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * File: main.js
+ * Module: GT511C3
+ * Author: Alberto Garbui
+ * Created: 24/02/15
+ * Version: 0.1
+ * Description: main module GT511C3
+ * Modification History:
+ ==============================================
+ * Version | Changes
+ ==============================================
+ * 0.1 File creation
+ ==============================================
+ */
+
+var GT511C3 = require('./lib/gt511c3');
+module.exports = GT511C3;
